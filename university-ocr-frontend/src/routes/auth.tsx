a9573954f44a3263dbb87d2c51df33ccd0378ae3
Maxlandry03 <landrymax041@gmail.com>
2026-06-29 20:05:22 +0100

    Update auth.tsx

--- university-ocr-frontend/src/routes/auth.tsx
+++ university-ocr-frontend/src/routes/auth.tsx
@@ -28,7 +28,7 @@ function AuthPage() {
       : { email, password, role };
 
     try {
-      const response = await fetch("http://localhost:8000/api/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
