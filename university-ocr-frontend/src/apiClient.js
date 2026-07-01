af71286736eee151b1c3d2f0496df6caf521a058
Maxlandry03 <landrymax041@gmail.com>
2026-07-01 14:31:12 +0100

    fix cors config cache

--- university-ocr-frontend/src/apiClient.js
+++ university-ocr-frontend/src/apiClient.js
@@ -1,7 +1,1 @@
-const apiClient = axios.create({
-    baseURL: 'https://university-administrative-project.onrender.com/api',
-    withCredentials: true,
-    headers: {
-        'Accept': 'application/json',
-    }
-});
+import axios from axios;
