cbbc6a2e6cfa9922ddcade5ab37254b01288ac87
Maxlandry03 <landrymax041@gmail.com>
2026-07-01 11:03:18 +0100

    Update Laravel Kernel and CORS settings for frontend integration

--- university-ocr-backend/config/cors.php
+++ university-ocr-backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['university-administrative-project-g.vercel.app'],
+    'allowed_origins' => ['https://university-administrative-project-g.vercel.app'],
 
     'allowed_origins_patterns' => [''],
 
